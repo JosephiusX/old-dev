98669d984ecfdeb49ef09f9fc338c74bf15f731b
Joseph <josephiusx@gmail.com>
2022-06-09 12:02:39 -0700

    108.  Wiring up Edit Expense

--- js/front/React-Andrew/expensify-app/src/components/EditExpensePage.js
+++ js/front/React-Andrew/expensify-app/src/components/EditExpensePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import {editExpense} from '../actions/expenses';
+import {editExpense, removeExpense} from '../actions/expenses';
 
 const EditExpensePage = props => {
-	console.log(props);
 	return (
 		<div>
 			<ExpenseForm
@@ -15,6 +14,14 @@ const EditExpensePage = props => {
 					// console.log('updated', expense);
 				}}
 			/>
+			<button
+				onClick={() => {
+					props.dispatch(removeExpense({id: props.expense.id}));
+					props.history.push('/');
+				}}
+			>
+				Remove
+			</button>
 		</div>
 	);
 };
